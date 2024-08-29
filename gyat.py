c4240fe2bb7072026439351918b2ebad9c6e2c96
Qu3rtox <aurel.widmer@students.lerbermatt.ch>
2024-08-29 15:51:44 +0200

    Update gyat.py

--- gyat.py
+++ gyat.py
@@ -4,5 +4,6 @@ while a != "nein" or a != "ja":
     a = input("Wer Gänsehaut, schlägt auch Enten. Ja oder nein? ")
 if a == "ja":
     print("Dönerbeule")
+    figg
 else:
     os.system("shutdown /p")
